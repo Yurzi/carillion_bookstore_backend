f1b20115085383927e36f7bd9b801887563a9136
Yurzi <luoxuanruo@hotmail.com>
2022-06-13 13:15:06 +0800

    [project:config] configure the project & create a app for store

--- carillion_bookstore/settings.py
+++ carillion_bookstore/settings.py
@@ -74,10 +74,18 @@ WSGI_APPLICATION = 'carillion_bookstore.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+        #    'default': {
+        #        'ENGINE': 'django.db.backends.sqlite3',
+        #        'NAME': BASE_DIR / 'db.sqlite3',
+        #    }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'carillion_bookstore',
+        'USER': 'root',
+        'PASSWORD': 'yurzi',
+        'HOST': '127.0.0.1',
+        'PORT': 3306,
+        }
 }
 
 
